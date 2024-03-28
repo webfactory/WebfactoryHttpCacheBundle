95bc37b6e051c6066be8e51afa47781dd6eeaa71
Matthias Pigulla <mp@webfactory.de>
2024-03-28 18:27:42 +0100

    Fix class references

--- src/NotModified/Annotation/ReplaceWithNotModifiedResponse.php
+++ src/NotModified/Annotation/ReplaceWithNotModifiedResponse.php
@@ -9,12 +9,14 @@
 
 namespace Webfactory\HttpCacheBundle\NotModified\Annotation;
 
+use Webfactory\HttpCacheBundle\NotModified\Attribute;
+
 /**
  * @Annotation
  *
  * @deprecated, to be replaced by attribute-based configuration
  */
-final class ReplaceWithNotModifiedResponse extends \Webfactory\HttpCacheBundle\NotModified\Attribute\ReplaceWithNotModifiedResponse
+final class ReplaceWithNotModifiedResponse extends Attribute\ReplaceWithNotModifiedResponse
 {
     public function __construct(array $parameters)
     {
@@ -22,8 +24,8 @@ final class ReplaceWithNotModifiedResponse extends \Webfactory\HttpCacheBundle\N
             'webfactory/http-cache-bundle',
             '1.4.0',
             'The %s annotation has been deprecated, use the %s attribute instead.',
-            NotModified\Annotation\ReplaceWithNotModifiedResponse::class,
-            NotModified\Attribute\ReplaceWithNotModifiedResponse::class
+            __CLASS__,
+            Attribute\ReplaceWithNotModifiedResponse::class
         );
 
         parent::__construct($parameters['value']);
